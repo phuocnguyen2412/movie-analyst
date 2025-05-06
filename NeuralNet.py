e84197197c7005f0a423a54bade622cfc595f7fd
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2025-05-06 14:59:41 +0700

    create data

---
+++ NeuralNet.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self, input_dim):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
